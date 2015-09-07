af19427dd9987f60ce69bda05510d726ae6c2742
Robert Elm <robert.elm@apdcomms.com>
2015-09-07 17:02:15 +0100

    Add template gruntfile, fix some paths

---
+++ content/gruntfile.js
@@ -0,0 +1,25 @@
+﻿/*jslint node: true */
+"use strict";
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                sourceMap:'content/styles.css.map'
+            },
+            dist: {
+                files: {
+                    'content/styles.css': 'content/styles.scss'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('build-Debug', ['sass']);
+    grunt.registerTask('build-Release', ['sass']);
+};
