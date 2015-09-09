ff5a76f69ef2ebbb5d1c48dcd3077687584ef611
Robert Elm <robert.elm@apdcomms.com>
2015-09-09 17:03:46 +0100

    fix more paths

--- content/node_bin/npm.cmd
+++ content/node_bin/npm.cmd
@@ -1,3 +1,3 @@
 @echo off
 SET PATH=%~dp0;%PATH%
-"%~dp0node" "%~dp0..\..\packages\MSBuild.Node.Local.1.0.0\node_modules\npm\bin\npm-cli.js" %*
+"%~dp0node" "%~dp0..\..\packages\MSBuild.Node.Local.1.0.0\tools\node_modules\npm\bin\npm-cli.js" %*
