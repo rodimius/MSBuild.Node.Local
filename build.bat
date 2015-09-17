513e4334211e7cd49c13f0efc6d21f8e3ef49e14
Robert Elm <robert.elm@apdcomms.com>
2015-09-17 16:52:24 +0100

    attempt to fix a pig's ear

---
+++ build.bat
@@ -0,0 +1,1 @@
+nuget pack .\MSBuild.Node.Local.nuspec -NoDefaultExcludes
--- content/gruntfile.js
+++ content/gruntfile.js
@@ -3,18 +3,28 @@
 
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            options: {
-                sourceMap:'content/styles.css.map'
-            },
-            dist: {
-                files: {
-                    'content/styles.css': 'content/styles.scss'
-                }
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      options: {
+        sourceMap:'content/styles.css.map'
+      },
+      dist: {
+        files: {
+          'content/styles.css': 'content/styles.scss'
         }
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'content',
+          src: ['*.css', '!*.min.css'],
+          dest: 'content',
+          ext: '.min.css'
+        }]
+      }
     });
 
     grunt.loadNpmTasks('grunt-sass');
